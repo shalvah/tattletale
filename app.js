fdeaa667af13ae33c347f5dd1c4b83d2fdb49a56
shalvah <diakon.ng@gmail.com>
2018-01-21 12:36:52 +0100

    Setup project and add packages

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+
+const index = require('./routes/index');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+module.exports = app;
