c5959145374f48df8dfe76118ab9e27628f3ba8f
shalvah <diakon.ng@gmail.com>
2018-01-21 15:26:25 +0100

    Add editing post capability

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,21 @@ const Post = require('./../models/post');
 
 router.get('/', (req, res, next) => {
     Post.find({}, {title: true}).exec((err, posts) => {
-        res.render('index', { posts });
+        res.render('index', {posts});
     });
 });
 
 router.get('/posts/:id', (req, res, next) => {
-    Post.findOne({ _id: req.params.id }).exec((err, post) => {
-        res.render('post', { post });
+    Post.findOne({_id: req.params.id}).exec((err, post) => {
+        res.render('post', {post});
     });
+});
+
+router.post('/posts/:id', (req, res, next) => {
+    Post.update({_id: req.params.id}, {body: req.body.body})
+        .exec((err, numberAffected) => {
+            res.send('');
+        });
 });
 
 module.exports = router;
